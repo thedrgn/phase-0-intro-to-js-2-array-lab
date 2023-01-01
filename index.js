c97d974cc46a081635421a0d2f0ae5a789667b48
thedrgn <118586751+thedrgn@users.noreply.github.com>
2022-12-31 21:38:22 -0500

    commit tree 3c0c814d4c3fdb0b8c4afe11c685da8a17fb7ddb

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your solution here!
+ const cats = [ "Milo", "Otis", "Garfield",]
+function destructivelyAppendCat(){cats.push('Ralph');}
+function destructivelyPrependCat(){cats.unshift('Bob');}
+function destructivelyRemoveLastCat(){cats.pop('Ralph');}
+function destructivelyRemoveFirstCat(){cats.shift('Bob');}
+function appendCat (){const copyofcats=[...cats,"Broom"];return copyofcats}
+function prependCat (){const copyofcats=["Arnold",...cats];return copyofcats}
+function removeLastCat(){const copyofCats= cats.slice(0,cats.length-1); 
+    return copyofCats
+}
+function removeFirstCat(){const copyofCats= cats.slice(1); 
+    return copyofCats
+}
